a859fa19e3a17af2d063bd6aade0b4692e58351f
QuyenLuong <ducquyenluong97@gmail.com>
2019-05-24 21:59:07 +0700

    Add address property for User

--- user.js
+++ user.js
@@ -2,6 +2,8 @@ function User() {
 	this.name = '';
 	this.age = 20;
 	this.gender = "Male";
+	this.address = "Nghe An province";
+	this.phone = "0977144135";
 }
 
 User.prototype.name = function(info) {
